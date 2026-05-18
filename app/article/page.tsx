09c44c46f55b434ae90fd364dbf866c66760839d
Gamer2Language <bagassatwi@gmail.com>
2026-05-18 09:00:32 +0700

    fixed dark mode on article and their child

--- app/article/page.tsx
+++ app/article/page.tsx
@@ -10,12 +10,12 @@ export default function Articles() {
         Cerita & Tips Pilihan
       </h3>
 
-      <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
+      <div className="md:grid-cols-4 grid grid-cols-1 gap-8">
         {ARTICLES.slice(0, 8).map((article) => (
           <Link
             key={article.id}
             href={`/article/${article.id}`}
-            className="border-slate-200 rounded-2xl hover:shadow-lg group flex flex-col overflow-hidden transition-shadow bg-white border"
+            className="border-border rounded-2xl hover:shadow-lg group bg-background flex flex-col overflow-hidden transition-shadow border"
           >
             {/* IMAGE */}
             <div className="relative h-56 overflow-hidden">
@@ -33,15 +33,15 @@ export default function Articles() {
 
             {/* CONTENT */}
             <div className="flex flex-col flex-1 p-6">
-              <h4 className="line-clamp-2 text-slate-800 mb-3 text-xl font-bold">
+              <h4 className="line-clamp-2 text-foreground mb-3 text-xl font-bold">
                 {article.title}
               </h4>
 
-              <p className="text-slate-600 line-clamp-3 mb-6 text-sm leading-relaxed">
+              <p className="text-foreground/80 line-clamp-3 mb-6 text-sm leading-relaxed">
                 {article.description}
               </p>
 
-              <div className="text-slate-500 border-slate-100 flex items-center justify-between pt-4 mt-auto text-xs font-medium border-t">
+              <div className="text-foreground/50 border-border flex items-center justify-between pt-4 mt-auto text-xs font-medium border-t">
                 <span>{article?.createdAt || "12 Okt 2023"}</span>
 
                 <span className="flex items-center gap-1.5">
