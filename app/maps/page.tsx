6fc7d5d98b5f8c4f31880e469698577462cf3dbd
mdwikurniawan976 <xyvnn478@gmail.com>
2026-05-19 11:21:37 +0700

    fix deploy

--- app/maps/page.tsx
+++ app/maps/page.tsx
@@ -1,7 +1,13 @@
-import { Control } from "./Control";
+"use client";
+import dynamic from "next/dynamic";
+
+const Control = dynamic(
+  () => import("./Control").then((mod) => mod.Control),
+  {
+    ssr: false,
+  }
+);
 
 export default function Maps() {
-  return (
-      <Control />
-  );
+  return <Control />;
 }
